1e3fffac3e06e7135ed3d92bef947c60b35c586e
djedai.genius@gmail.com <djedai.genius@gmail.com>
2017-10-11 20:08:57 +0300

    check task

--- src/app/day/task-item/task-item.component.ts
+++ src/app/day/task-item/task-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {ApiService} from '../../services/api.service';
+import {ISubscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-task-item',
@@ -9,6 +10,8 @@ export class TaskItemComponent implements OnInit {
   @Input() task;
   active: boolean = false;
 
+  private checkSubscribe: ISubscription;
+
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
@@ -16,11 +19,12 @@ export class TaskItemComponent implements OnInit {
   }
 
   onCheck() {
-    this.apiService.checkTask(this.task.id, !this.active)
+    this.checkSubscribe = this.apiService.checkTask(this.task.id, !this.active)
         .subscribe(
         (task) => {
           console.log(task);
           this.active = task.checked;
+          this.checkSubscribe.unsubscribe();
         }
     );
   }
