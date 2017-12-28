801f67ba56b1d7329d203dc427c8713b90619ab9
genius <djedai.genius@gmail.com>
2017-12-28 14:57:34 +0300

    Week Cycle complete

--- src/app/weekcycle/weekcycle.component.ts
+++ src/app/weekcycle/weekcycle.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from '../services/api.service';
+import {NgForm} from '@angular/forms';
+import {Subject} from 'rxjs/Subject';
 
 @Component({
   selector: 'app-weekcycle',
@@ -6,10 +9,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weekcycle.component.css']
 })
 export class WeekcycleComponent implements OnInit {
+  tasks;
+  reward = new Subject();
+  constructor(private apiService: ApiService) {
+    const id = localStorage.getItem('id');
+    this.apiService.getWeekCycle(id)
+      .subscribe( (cycle) => {
+        console.log(cycle['0'].id);
+        const cycle_id = cycle['0'].id;
+        this.apiService.getMyRewards(cycle_id)
+          .subscribe((reward) => {
+          this.reward = reward['0'];
+          console.log(reward);
+        });
+        this.apiService.getMyTasks(cycle_id)
+          .subscribe((tasks) => {
+            this.tasks = tasks;
+            console.log(tasks);
+          });
+      });
+  }
 
-  constructor() { }
+  onCheck(id, checked, i) {
+    this.apiService.checkTask(id, !checked)
+        .subscribe(
+        (task) => {
+          console.log(task);
+          this.tasks.active = task['checked'];
+          this.tasks[i].checked = task['checked'];
+          }
+    );
+  }
 
   ngOnInit() {
   }
+
+  update (form: NgForm, i) {
+    const id = this.tasks[i].id;
+    this.apiService.updateTask(form.value.text, id).subscribe(
+      (step) => {
+        this.tasks[i].text = step.text;
+      }
+    );
+  }
+
+  updateReward (id, form: NgForm) {
+    this.apiService.updateReward(id, form.value.text)
+      .subscribe((reward) => {
+        console.log(reward);
+        this.reward['text'] = reward.text;
+      });
+  }
+
 
 }
